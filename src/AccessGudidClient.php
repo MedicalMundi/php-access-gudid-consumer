dbbf087d8d2b9702dd7606301419ce862216a60e
zero <teclaizerai@gmail.com>
2020-03-24 20:17:44 +0100

    Setup AccessGudidClient class.

--- src/AccessGudidClient.php
+++ src/AccessGudidClient.php
@@ -4,6 +4,21 @@ declare(strict_types=1);
 
 namespace MedicalMundi\AccessGudid;
 
+use Http\Client\HttpClient;
+use Http\Discovery\HttpClientDiscovery;
+
 class AccessGudidClient
 {
+    /**
+     * @var HttpClient
+     */
+    private $httpClient;
+
+    /**
+     * @param HttpClient|null $httpClient Client to do HTTP requests, if not set, auto discovery will be used to find a HTTP client.
+     */
+    public function __construct(HttpClient $httpClient = null)
+    {
+        $this->httpClient = $httpClient ?: HttpClientDiscovery::find();
+    }
 }
