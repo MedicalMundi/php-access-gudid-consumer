9d9af97b1c8d96106d141ad780a3d3d6aaf0cc00
zerai <teclaizerai@gmail.com>
2022-10-15 19:07:30 +0200

    Update ECSConfig.

--- ecs.php
+++ ecs.php
@@ -9,60 +9,41 @@ use PhpCsFixer\Fixer\PhpTag\BlankLineAfterOpeningTagFixer;
 use PhpCsFixer\Fixer\PhpTag\LinebreakAfterOpeningTagFixer;
 use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
 use PhpCsFixer\Fixer\Strict\StrictComparisonFixer;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
-
-    $parameters->set(Option::PATHS, [
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
+        __DIR__ . '/ecs.php',
     ]);
 
-    $parameters->set(
-        'skip',
-        [
-            BlankLineAfterOpeningTagFixer::class,
-            LinebreakAfterOpeningTagFixer::class,
-        ]
-    );
+    $ecsConfig->skip([
+        BlankLineAfterOpeningTagFixer::class,
+        LinebreakAfterOpeningTagFixer::class,
+    ]);
 
-    $containerConfigurator->import(SetList::SPACES);
-    $containerConfigurator->import(SetList::PSR_12);
+    $ecsConfig->rules([
+        DeclareStrictTypesFixer::class,
+        BlankLineAfterNamespaceFixer::class,
+        NoUnusedImportsFixer::class,
+        LinebreakAfterOpeningTagFixer::class,
+        //OrderedImportsFixer::class,
+        //NativeFunctionInvocationFixer::class,
+        //FullyQualifiedStrictTypesFixer::class,
+        StrictComparisonFixer::class,
 
-//    $parameters->set(Option::SETS, [
-//        SetList::SPACES,
-//        //SetList::ARRAY,
-//        //SetList::DOCBLOCK,
-//        //SetList::NAMESPACES,
-//        //SetList::CONTROL_STRUCTURES,
-//        //SetList::CLEAN_CODE,
-//        SetList::PSR_12,
-//    ]);
+    ]);
 
+    $ecsConfig->ruleWithConfiguration(ArraySyntaxFixer::class, [
+        'syntax' => 'short',
+    ]);
 
-    $services = $containerConfigurator->services();
-
-    $services->set(DeclareStrictTypesFixer::class);
-    //$services->set(LinebreakAfterOpeningTagFixer::class);
-
-    $services->set(BlankLineAfterNamespaceFixer::class);
-
-    $services->set(NoUnusedImportsFixer::class);
-
-    $services->set(StrictComparisonFixer::class);
-
-
-    $services->set(ArraySyntaxFixer::class)
-        ->call('configure', [[
-            'syntax' => 'short',
-        ]]);
-
-    // run and fix, one by one
-    $containerConfigurator->import(SetList::SPACES);
-    // $containerConfigurator->import(SetList::ARRAY);
-    // $containerConfigurator->import(SetList::DOCBLOCK);
-    $containerConfigurator->import(SetList::PSR_12);
+    $ecsConfig->sets([
+        SetList::SPACES,
+//        SetList::ARRAY,
+//        SetList::DOCBLOCK,
+        SetList::PSR_12,
+    ]);
 };
